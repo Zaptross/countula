11df2f9394ba9a7c4ad3ea1a6b590d26f93bc9c1
M Price <zaptross@outlook.com>
2024-01-28 19:45:09 +1000

    feat(db): add rule_settings table

--- internal/database/database.go
+++ internal/database/database.go
@@ -34,7 +34,7 @@ func Connect(config DatabaseConfig) *gorm.DB {
 		panic(err)
 	}
 
-	db.AutoMigrate(&Turn{}, &AuditLog{}, &StatisticRow{}, &ServerConfig{})
+	db.AutoMigrate(&Turn{}, &AuditLog{}, &StatisticRow{}, &ServerConfig{}, &RuleSetting{})
 
 	return db
 }
